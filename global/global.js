2a4e3bd0baf2504791c28320af1b0646b8572d48
AynurSalimli <aynur.selimli@ascca.edu.az>
2023-03-14 16:14:00 +0400

    aos animation added 1 page

--- global/global.js
+++ global/global.js
@@ -1,5 +1,6 @@
 const bar = document.querySelector(".hamburger");
 const menu = document.querySelector(".header__menu");
+const loader = document.querySelector(".loader--content");
 
 function toggleMenu() {
   menu.classList.toggle("active");
@@ -45,28 +46,17 @@ function mode() {
   }
 }
 
-//! end dark/light mode
 
-// const refreshBtn = document.querySelector("#refresh");
-// const toggleBtn = document.querySelector("#toggle");
-// const clearBtn = document.querySelector("#clear");
-// const theme = localStorage.getItem("theme");
 
-// theme === "dark" && document.body.classList.add("dark")
 
-// // toggleBtn.addEventListener("click", () => {
-// //     document.body.classList.toggle("dark")
-// //     if (theme === "dark") {
-// //         localStorage.setItem("theme", "light");
-// //     } else {
-// //         localStorage.setItem("theme", "dark");
-// //     }
-// // })
 
-// // refreshBtn.addEventListener("click", () => {
-// //     location.reload()
-// // })
+//! delete loader
 
-// // clearBtn.addEventListener("click", () => {
-// //     localStorage.clear()
-// // })
+function removeLoader() {
+  setTimeout(() => {
+    loader.classList.add("fade");
+  }, 2000);
+  setTimeout(() => {
+    loader.remove();
+  }, 2500);
+}
